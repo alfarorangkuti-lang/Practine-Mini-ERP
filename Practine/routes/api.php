0e4fdc41eadd1d990721870419308a9949eadf95
alfarorangkuti-lang <alfarorangkuti@gmail.com>
2026-02-10 10:55:55 +0700

    frontend push & smtp gmail setup

--- Practine/routes/api.php
+++ Practine/routes/api.php
@@ -6,3 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::middleware(['auth:sanctum'])->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::middleware(['auth:sanctum', 'verified'])->group(function () {
+    Route::get('/dashboard', function () {
+        return "Verified User Only";
+    });
+});
